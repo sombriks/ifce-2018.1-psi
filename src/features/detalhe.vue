c6d84dcdc737ac8ae4b136e48ec29ce3c012ee3b
tavares1 <lucastavaresvasconcelos@gmail.com>
2018-04-12 00:48:22 -0300

    Refatorando detalhes, escolhi utilizar vuefire como objeto

--- src/features/detalhe.vue
+++ src/features/detalhe.vue
@@ -3,56 +3,54 @@
     <v-container fill-height>
         <v-layout align-center>
           <v-flex>
-            <img :src="imagesRef">
-            <h3 class="display-1">{{tituloAnuncio}}</h3>
-              <p v-if=recompensa class="subheading">Recompensa : {{recompensa}}</p>
-            <span class="body-1"> {{descricaoAnuncio}} </span>
+            <h3 class="display-1">{{this.anuncios[this.key].nomeanuncio}}</h3>
+            <p v-if="this.anuncios[this.key].recompensa" class="subheading">Recompensa : {{this.anuncios[this.key].recompensa}}</p>
+            <span class="body-1"> {{this.anuncios[this.key].descricaoanuncio}} </span>
+            <v-list two-line>
+              <template  v-for="(value,key) in this.anuncios[this.key].comentario">
+              <v-list-tile avatar :key="key">
+                <v-list-tile-content>
+                  <v-list-tile-title v-html="key"></v-list-tile-title>
+                  <v-list-tile-sub-title v-html="value"></v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </template>
+            </v-list>
           </v-flex>
         </v-layout>
       </v-container>
-    <v-carousel>
-      <v-carousel-item v-for="(item,i) in items" :src="item.src" :key="i">
-      </v-carousel-item>
-    </v-carousel>
-  </div>
+   </div>
   </template>
 
 <script>
-const { firebase,firebaseapp,storage,db } = require("../components/config")
+const { db } = require("../components/config")
 module.exports = {
   name: "Detalhe",
-  firebase () {
-    return {
-       anuncio: db.ref("anuncios").child(this.key)
-    }
-  },
-  created(){
-    this.formatAnuncio();
-    this.getImgs();
+  firebase:{
+      anuncios: { 
+        source: db.ref(`anuncios`),
+        asObject:true
+      }
+    },
+  created() {
+    this.$store.commit("setTitle", "Find My Pet - Últimos anúncios")
   },
   data () {
     return {
       key : this.$route.params.idanuncionanimal,
       items: [],
-      tituloAnuncio : "",
-      descricaoAnuncio : "",
-      recompensa : null
+      nomeanuncio:{}
       }
   },
   methods: {
-    getImgs(){
-    let storage = firebaseapp.storage();
-    let storageRef = storage.ref();
-    let imagesRef = storageRef.child('cachorros/pexels-photo-316776.jpeg').getDownloadURL().then((url) => {
-        this.items.push({src: url})
-        console.log(this.items[0].src)
-      });
-    },
-    formatAnuncio(){
-      this.tituloAnuncio = this.anuncio[1]['.value']
-      this.descricaoAnuncio = this.anuncio[0]['.value']
-      this.recompensa =  this.anuncio[2]['.value']
-    }
+    // getImgs(){
+    // let storage = firebaseapp.storage();
+    // let storageRef = storage.ref();
+    // let imagesRef = storageRef.child('cachorros/pexels-photo-316776.jpeg').getDownloadURL().then((url) => {
+    //     this.items.push({src: url})
+    //     console.log(this.items[0].src)
+    //   });
+    // }
   }
 }
 </script>
